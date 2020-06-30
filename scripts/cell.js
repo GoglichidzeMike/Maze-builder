4205bb73225d468c13c5dd4d1ff02c302bc7b074
GoglichidzeMike <goglichidze.mike@gmail.com>
2020-06-30 14:52:30 +0400

    commiting files

---
+++ scripts/cell.js
@@ -0,0 +1,73 @@
+//cell constructor class/function
+function Cell(i, j) {
+  //i and j are coordinates
+  this.i = i;
+  this.j = j;
+  //walls array, boolean to show/hide walls 
+  //            TOP   RIGHT BOTT  LEFT
+  this.walls = [true, true, true, true]
+  this.visitsed = false;
+
+
+  this.highlight = function () {
+    let x = this.i * w;
+    let y = this.j * w;
+    noStroke();
+    fill(0, 200, 0);
+    rect(x, y, w, w);
+  }
+
+  this.checkNeighbors = function () {
+    let neighbors = [];
+
+    let right = grid[index(i + 1, j)];
+    let top = grid[index(i, j - 1)];
+    let bottom = grid[index(i, j + 1)];
+    let left = grid[index(i - 1, j)];
+
+    if (top && !top.visited) {
+      neighbors.push(top);
+    }
+    if (right && !right.visited) {
+      neighbors.push(right);
+    }
+    if (bottom && !bottom.visited) {
+      neighbors.push(bottom);
+    }
+    if (left && !left.visited) {
+      neighbors.push(left);
+    }
+
+    if (neighbors.length > 0) {
+      let r = floor(random(0, neighbors.length))
+      return neighbors[r];
+    } else {
+      return undefined;
+    }
+  }
+
+
+  this.show = function () {
+    let x = this.i * w;
+    let y = this.j * w;
+    stroke(0, 0, 0);
+    if (this.walls[0]) {
+      line(x, y, x + w, y);
+    }
+    if (this.walls[1]) {
+      line(x + w, y, x + w, y + w);
+    }
+    if (this.walls[2]) {
+      line(x + w, y + w, x, y + w);
+    }
+    if (this.walls[3]) {
+      line(x, y + w, x, y);
+    }
+    //color visited cells
+    if (this.visited) {
+      noStroke();
+      fill(255, 200, 200);
+      rect(x, y, w, w, );
+    }
+  }
+}
---
+++ scripts/main.js
@@ -0,0 +1,105 @@
+let cols, rows
+//row size constant
+
+
+let w = 60;
+let fRate = 50;
+//grids empty array
+let current;
+let grid = [];
+let stack = [];
+
+$(document).ready(function () {
+
+  console.log($('speed').val());
+
+});
+
+
+
+//setup function creates canvas, defines assigns ros and cols amounts.
+function setup() {
+  let canvas = createCanvas(600, 600);
+  canvas.parent('sketch-holder');
+
+  cols = floor(width / w);
+  rows = floor(height / w);
+
+  frameRate(fRate);
+  //create cell rows times columns and push to array , 100 cells
+
+
+  for (let j = 0; j < rows; j++) {
+    for (let i = 0; i < cols; i++) {
+      let cell = new Cell(i, j);
+      grid.push(cell);
+    }
+  }
+
+  current = grid[0];
+}
+
+
+
+//draws 100 cells onto the canvas
+function draw() {
+  background(50);
+  for (let i = 0; i < grid.length; i++) {
+    grid[i].show();
+  }
+
+
+  current.visited = true;
+  current.highlight();
+
+
+  // STEP 1
+  let next = current.checkNeighbors();
+  if (next) {
+    next.visited = true;
+
+    //STEP 2
+    stack.push(current);
+
+
+    // STEP 3
+
+    removeWalls(current, next);
+
+
+    // STEP 4
+    current = next;
+  } else if (stack.length > 0) {
+    current = stack.pop();
+  }
+
+}
+
+
+function index(i, j) {
+  if (i < 0 || j < 0 || i > cols - 1 || j > rows - 1) {
+    return -1;
+  }
+  return i + j * cols;
+}
+
+
+
+function removeWalls(a, b) {
+  let x = a.i - b.i;
+  if (x === 1) {
+    a.walls[3] = false;
+    b.walls[1] = false;
+  } else if (x === -1) {
+    a.walls[1] = false;
+    b.walls[3] = false;
+  }
+  let y = a.j - b.j;
+  if (y === 1) {
+    a.walls[0] = false;
+    b.walls[2] = false;
+  } else if (y === -1) {
+    a.walls[2] = false;
+    b.walls[0] = false;
+  }
+}
